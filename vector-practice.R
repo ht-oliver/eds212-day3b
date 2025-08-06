25c9f818a907cea4894eb5e19beab667340494be
ht-oliver <holiver327@gmail.com>
2025-08-06 13:51:47 -0700

    added vectors of multiple types

---
+++ vector-practice.R
@@ -0,0 +1,13 @@
+
+# Create and store the vector
+
+marmots <- c("blue", "green", "4", "yellow")
+class(marmots)
+
+# Create and store numeric vector
+pika <- c(12.4, 6.8, 2.9, 8.5)
+class(pika)
+
+# Create and store integer vector
+bear <- c(20L, 3L, 5L, 18L, 23L)
+class(bear)
